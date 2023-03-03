b7458c8cf05622dfbdab24124ad51b5891e770fa
nareshmanthatti <120558781+nareshmanthatti@users.noreply.github.com>
2023-03-03 20:53:03 +0530

    Add files via upload

---
+++ Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Logo=()=>{
+    return(
+        <div>
+         <img src="https://www.carlogos.org/car-logos/porsche-logo-2100x1100.png" alt="" />
+        </div>
+    )
+}
+export default Logo
