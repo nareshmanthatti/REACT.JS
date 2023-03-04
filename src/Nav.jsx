30f0080cbbbd77fb28006487e26255fc443fb28c
nareshmanthatti <120558781+nareshmanthatti@users.noreply.github.com>
2023-03-04 14:52:43 +0530

    Add files via upload

---
+++ src/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from './components/Logo';
+import Menu from './components/Menu';
+ 
+const Nav=()=>{
+    return(
+        <div className="mainBlock">
+        <Logo/>
+        <Menu/>
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/components/Menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu=()=>{
+    return(
+        <div className="menuBlock">
+            <ul>
+                <li><a href="">HOME</a> </li>
+                <li><a href="">LOGIN</a> </li>
+                <li><a href="">ABOUT</a> </li>
+            </ul>
+        </div>
+    )
+}
+export default Menu
